66c085519e435cf42aa8e1212b67da387cfe3c99
Enzo Nicaise <enzonicaise.1612@gmail.com>
2023-08-07 22:11:29 +0200

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      lightBlue: "#E7F2F8",
+      blue: "#74BDCB",
+      orange: "#FFA384",
+      brown: "#EFE7BC",
+      white: "FFFF",
+      black: "191919",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
